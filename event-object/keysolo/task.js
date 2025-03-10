13d83a57fe7bb62642e2799945dec6ed06e78610
Bondarev Artem <bond210484@mail.ru>
2025-03-10 17:03:33 +0700

    2 task finish

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,17 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+
+    document.addEventListener('keydown', event => {
+      const chr = event.key
+      //console.log(chr)
+    
+    if (chr === this.currentSymbol.textContent) {
+      this.success();
+  } else {
+      this.fail();
+  }
+})
   }
 
   success() {
